5f367bea4c6f2731b59b06cfa1646b86a1d00cef
JD <judsonwebdesign@gmail.com>
2024-03-02 05:47:52 -0700

    readJson flattens now

--- src/readJson.py
+++ src/readJson.py
@@ -2,6 +2,14 @@ import subprocess
 import json
 import sys
 
+def flatten_json(json_obj, flat_json={}):
+    for key in json_obj:
+        if isinstance(json_obj[key], dict):
+            flatten_json(json_obj[key], flat_json)
+        else:
+            flat_json[key] = json_obj[key]
+    return flat_json
+
 def read_pcapng_file(pcapng_file, on_packet):
   tshark = subprocess.Popen(['tshark', '-r', pcapng_file, '-T', 'json'], stdout=subprocess.PIPE)
   json_buffer = ''
@@ -14,7 +22,7 @@ def read_pcapng_file(pcapng_file, on_packet):
       if json_buffer.endswith(','):
         json_buffer = json_buffer[:-1]
       packet = json.loads(json_buffer)
-      on_packet(packet)
+      on_packet(flatten_json(packet))
       json_buffer = ''
   return packets
 
