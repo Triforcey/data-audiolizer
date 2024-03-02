2acfe3858a9834c3250fa8c9fb03f50a3c5f0452
Kaden Hart <a02366638@usu.edu>
2024-03-01 23:45:37 -0700

    lksjdbfklsadjg

--- src/testReadJson.py
+++ src/testReadJson.py
@@ -1,24 +1,15 @@
-import numpy as np
-import scipy.io.wavfile as wavf
-# Open the WAV file
-wav_file = open("StarWars3.wav", "rb")
-
-# Read the data from the file
-data = np.fromfile(wav_file, dtype=np.int16)
-
-# Close the file
-wav_file.close()
-
-# Print the data
-# with np.printoptions(threshold=np.inf):
-#     print(data)
 
 if __name__ == "__main__":
 
     from scipy.io.wavfile import write
     import numpy as np
+    import json
+    packetdata ={}
+    with open('C:\\Users\\kaden\\Data Audiolizer\\data-audiolizer\\src\\oopsdroppedthis.json', 'r') as f:
+        packetdata = json.load(f)
     samplerate = 44100; fs = 100
     t = np.linspace(0., 1., samplerate)
     amplitude = np.iinfo(np.int16).max
     data = amplitude * np.sin(2. * np.pi * fs * t)
+    print(data)
     write("example.wav", samplerate, data.astype(np.int16))
