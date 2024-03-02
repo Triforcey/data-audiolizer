e2718e9a3fb3d5cce3d28ff959b97fe2153e6b94
Kaden Hart <a02366638@usu.edu>
2024-03-02 00:30:52 -0700

    waveeessss

--- src/testReadJson.py
+++ src/testReadJson.py
@@ -10,9 +10,16 @@ if __name__ == "__main__":
     packetdata ={}
     with open(dir_path + '/oopsdroppedthis.json', 'r') as f:
         packetdata = json.load(f)
-    samplerate = 44100; fs = 100
-    t = np.linspace(0., 1., samplerate)
-    amplitude = np.iinfo(np.int16).max
+
+    packetLength = float(packetdata['_source']['layers']['wlan_radio']['wlan_radio.duration'])/10
+    numWaves = .25
+    # print(packetLength)
+    samplerate = 44100
+    fs = 1/packetLength
+    t = np.linspace(0., 1.*packetLength, int(samplerate*packetLength))
+    amplitude = np.iinfo(np.int32).max
     data = amplitude * np.sin(2. * np.pi * fs * t)
+    data = data[:int(len(data)*numWaves)]
+
     print(data)
     write("example.wav", samplerate, data.astype(np.int16))
